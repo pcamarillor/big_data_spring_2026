080e833a3897b895bc61bed201be2cebea119c84
sebastian0023 <sebastian.macias@iteso.mx>
2026-01-28 19:13:13 -0600

    Update build-images.sh

--- infra/spark-cluster/build-images.sh
+++ infra/spark-cluster/build-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-
+#jandicjnaiejnc
 docker build -t spark-base:latest ./base
 docker build -t spark-master:latest ./spark-master
 docker build -t spark-worker:latest ./spark-worker
