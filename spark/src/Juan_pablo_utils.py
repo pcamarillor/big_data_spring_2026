3b4f8fd390f7c8f7005b91d292dbcd359e68a770
jpperezl2003 <jpperezl2003@gmail.com>
2026-02-05 14:37:41 -0600

    Utils en branch

---
+++ spark/src/Juan_pablo_utils.py
@@ -0,0 +1,12 @@
+class student:
+    def __init__(self,name,grade_1):
+        self.name=name
+        self.grades= []
+        self.grade_1=grade_1
+    
+    def display(self):
+        print(f"Student: {self.name}")
+        print(f"Grade 1: {self.grade_1}")
+        
+    def __repr__(self):
+        pass
