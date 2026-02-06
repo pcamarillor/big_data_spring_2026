6bafc414854aa70f88bf5c7d8007f35ee58d459f
Pablo Camarillo <p.camarillor@gmail.com>
2026-02-05 21:15:27 -0600

    Add examples of OOP and functional programming

---
+++ spark/src/utils.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__(self, name, grade1):
+        self._name = name
+        self._grades = []
+        self._grades.append(grade1)
+
+    def __repr__(self):
+        return f"Student(name={self._name})"
+
+
+    def display(self):
+        print(f"name:{self._name}")
+        for g in self._grades:
+            print(f"grade:{g}")
+        
