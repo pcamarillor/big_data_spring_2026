61f186aca7b05d512b22b14b1775b89ad8330fa4
Niconavarrov <nicolas.navarro@iteso.mx>
2026-02-07 21:26:07 -0600

    Lab02 Completed

--- spark/src/nico_utils.py
+++ spark/src/nico_utils.py
@@ -1,7 +1,50 @@
-class Student:
-    def __init__(self, name, degree):
-        self.name = name
-        self.degree = degree
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        super().__init__()
+
+        if initial_balance < 0:
+            print("WARNING: Initial balance cannot be negative. New balance = 0.")
+            initial_balance = 0
+        self.balance = initial_balance
+        self.operations = {
+            "deposit": self.deposit,
+            "withdraw": self.withdraw,
+            "get_balance": self.get_balance
+        }
+
+    def deposit(self, amount):
+        if amount < 0:
+            print("WARNING: Deposit amount cannot be negative")
+            return False
+        
+        self.balance += amount
+        print(f"{amount} deposited. New balance: {self.balance}")
+        return True
+
+    def withdraw(self, amount):
+        if amount <= 0:
+            print("WARNING: Withdrawal amount cannot be negative or null")
+            return False
+        if amount > self.balance:
+            print("WARNING: Insufficient funds")
+            return False
+        
+        self.balance -= amount
+        print(f"{amount} withdrawn. New balance: {self.balance}")
+        return True
     
-    def information(self):
-        print(f"HOLA CHAMBAMASTERS, LLEGO A USTEDES {self.name} DESDE {self.degree}")
+    def get_balance(self):
+        print(f"Current balance: {self.balance}")
+        return self.balance
+
+    def process(self, operation, amount=0):
+        if operation not in self.operations:
+            print("WARNING: Invalid operation")
+            return False
+        
+        operation_func = self.operations[operation]
+
+        if amount != 0:
+            return operation_func(amount)
+        else:
+            return operation_func()
