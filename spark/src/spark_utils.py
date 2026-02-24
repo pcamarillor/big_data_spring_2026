ad686d31783cc299083d6edc10817189ca85889a
Niconavarrov <nicolas.navarro@iteso.mx>
2026-02-23 20:23:38 -0600

    Lab03 Completed on this branch

--- spark/src/spark_utils.py
+++ spark/src/spark_utils.py
@@ -1,20 +1,9 @@
 import findspark
 findspark.init()
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructField, \
-                                StringType, \
-                                StructType, \
-                                IntegerType, \
-                                LongType, \
-                                ShortType, \
-                                DoubleType, \
-                                FloatType, \
-                                BooleanType, \
-                                DateType, \
-                                TimestampType, \
-                                BinaryType, \
-                                ArrayType, \
-                                MapType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
+from pyspark.sql.types import StructType, StringType, IntegerType, IntegerType, LongType, ShortType, DoubleType, FloatType, BooleanType, DateType, FloatType, BooleanType, DateType, TimestampType, BinaryType, StructField, ArrayType
+from pyspark.sql.functions import when, count, isnull, avg, col
 
 
 class SparkUtils:
@@ -24,9 +13,53 @@ class SparkUtils:
                     .master(master_url) \
                     .config("spark.ui.port", "4040") \
                     .getOrCreate()
-    
+   
     def __repr__(self):
         return str(self._spark.sparkContext)
-    
+   
     def spark_context(self):
         return self._spark.sparkContext
+ 
+
+    @staticmethod
+    def generate_schema(columns_info) -> StructType:
+        """
+        Generates a list of StructField objects from a list of tuples.
+
+        Args:
+            column_info (list of tuples): Each tuple contains (column_name, data_type_string).
+
+        Returns:
+            list: A list of StructField objects.
+        """
+        # Mapping from string type names to PySpark data types
+        type_mapping = {
+            "string": StringType(),
+            "int": IntegerType(),
+            "long": LongType(),
+            "short": ShortType(),
+            "double": DoubleType(),
+            "float": FloatType(),
+            "boolean": BooleanType(),
+            "date": DateType(),
+            "timestamp": TimestampType(),
+            "binary": BinaryType(),
+            "array_int": ArrayType(IntegerType()),
+            "array_string": ArrayType(StringType()),
+            "struct": StructType()
+        }
+
+        struct_fields = []
+        for column_info in columns_info:
+            if column_info[1] not in type_mapping:
+                raise ValueError(f"Unsupported data type: {column_info[1]}")
+
+            # Create a StructField for the column
+            struct_field = StructField(column_info[0], type_mapping[column_info[1]], True)
+            struct_fields.append(struct_field)
+
+        return StructType(struct_fields)
+    
+    @staticmethod
+    def null_count(df):
+        return df.select([count(when(isnull(c), c)).alias(c) for c in df.columns])
