1dc773aa109e1aa4bbc98559289bd8f08c8345ab
Santiago Villa <santiagovillardz@gmail.com>
2026-03-02 18:36:12 -0600

    Uploading Lab 04

--- spark/src/spark_utils.py
+++ spark/src/spark_utils.py
@@ -5,7 +5,7 @@ findspark.init()
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType, IntegerType, LongType, \
                             ShortType, DoubleType, FloatType, BooleanType, \
-                            DateType, TimestampType, BinaryType, ArrayType, MapType, StructType
+                            DateType, TimestampType, BinaryType, ArrayType, StructType, StructField
 
 class SparkUtils:
     def __init__(self, master_url, appname):
@@ -25,6 +25,42 @@ class SparkUtils:
     def stop(self):
         self._spark.sparkContext.stop()
 
-    # @staticmethod
-    # def generate_schema(data) -> StructType:
+    @staticmethod
+    def generate_schema(columns_info) -> StructType:
+        """
+        Generates a list of StructField objects from a list of tuples.
+
+        Args:
+            column_info (list of tuples): Each tuple contains (column_name, data_type_string).
+
+        Returns:
+            list: A list of StructField objects.
+        """
+        # Mapping from string type names to PySpark data types
+        type_mapping = {
+            "string": StringType(),
+            "int": IntegerType(),
+            "long": LongType(),
+            "short": ShortType(),
+            "double": DoubleType(),
+            "float": FloatType(),
+            "boolean": BooleanType(),
+            "date": DateType(),
+            "timestamp": TimestampType(),
+            "binary": BinaryType(),
+            "array_int": ArrayType(IntegerType()),
+            "array_string": ArrayType(StringType()),
+            "struct": StructType()
+        }
+
+        struct_fields = []
+        for column_info in columns_info:
+            if column_info[1] not in type_mapping:
+                raise ValueError(f"Unsupported data type: {column_info[1]}")
+
+            # Create a StructField for the column
+            struct_field = StructField(column_info[0], type_mapping[column_info[1]], True)
+            struct_fields.append(struct_field)
+
+        return StructType(struct_fields)
         
