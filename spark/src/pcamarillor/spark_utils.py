b703dfc0186f229e7aafbf1dda87db08a7b0c3f4
Pablo Camarillo <p.camarillor@gmail.com>
2026-03-18 18:10:26 -0600

    Include examples for lecture 14

--- spark/src/pcamarillor/spark_utils.py
+++ spark/src/pcamarillor/spark_utils.py
@@ -7,21 +7,22 @@ from pyspark.sql.functions import count, when, isnull
 
 class SparkUtils:
 
-    def __init__(self, app_name, master_url, spark_jars=None):
+    def __init__(self, app_name, master_url, spark_jars=None, spark_packages=None):
+        spark_builder = (
+            SparkSession.builder
+            .appName(app_name)
+            .master(master_url)
+            .config("spark.ui.port", "4040")
+        )
         if spark_jars is not None:
-           self._spark = (SparkSession.builder
-                .appName(app_name)
-                .master(master_url)
-                .config("spark.ui.port", "4040")
-                .config("spark.jars", spark_jars)
-                .getOrCreate())
-        else:
-            self._spark = SparkSession.builder \
-                .appName(app_name) \
-                .master(master_url) \
-                .config("spark.ui.port", "4040") \
-                .getOrCreate()
-    
+            spark_builder = spark_builder.config("spark.jars", spark_jars)
+
+        if spark_packages is not None:
+            spark_builder = spark_builder.config("spark.jars.packages", spark_packages)
+
+        self._spark = spark_builder.getOrCreate()
+        self._spark.conf.set("spark.sql.shuffle.partitions", "5")
+
     @property
     def spark(self):
         return self._spark
